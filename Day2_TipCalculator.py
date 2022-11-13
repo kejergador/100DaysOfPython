9c765385f0bff3c95802bf55dcf8fc108a388c75
kejergador <99165959+kejergador@users.noreply.github.com>
2022-11-14 01:55:54 +0800

    Update Day2_TipCalculator.py
    changed from round to format

--- Day2_TipCalculator.py
+++ Day2_TipCalculator.py
@@ -4,5 +4,5 @@ tip = int(input('What percentage tip would you like to give? 10,12 or 15? '))
 cus_num = int(input('How many people to split the bill? '))
 
 
-payment_per_person = round((total_bill/cus_num)*(1+(tip/100)),2)
+payment_per_person = "{:.2f}".format((total_bill/cus_num)*(1+(tip/100)))
 print(f'Each person should pay ${payment_per_person}')
