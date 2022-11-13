c2b441951ea79e2928b8603dbf8bb759b5eb8768
kejergador <99165959+kejergador@users.noreply.github.com>
2022-11-14 01:50:15 +0800

    Create Day2_TipCalculator.py

---
+++ Day2_TipCalculator.py
@@ -0,0 +1,8 @@
+print('Welcome to the tip calculator\n\n\n')
+total_bill = float(input('What was the total bill? $'))
+tip = int(input('What percentage tip would you like to give? 10,12 or 15? '))
+cus_num = int(input('How many people to split the bill? '))
+
+
+payment_per_person = round((total_bill/cus_num)*(1+(tip/100)),2)
+print(f'Each person should pay ${payment_per_person}')
