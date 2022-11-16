e538b53dadf5ed3b912000cfc7b5d0889b7ad4c0
kejergador <99165959+kejergador@users.noreply.github.com>
2022-11-16 14:24:53 +0800

    Create Day4_Ex3_TreasureMap.py

---
+++ Day4_Ex3_TreasureMap.py
@@ -0,0 +1,17 @@
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+
+
+position1 = int(position[0])
+position2 = int(position[1])
+map[position2-1][position1-1] = "X"
+
+
+
+print(f"{row1}\n{row2}\n{row3}")
